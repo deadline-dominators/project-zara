4311dad8ac80b70c419821fb9a2f00389815e4ad
zeinebkhemida <zeineb.khemida11@gmail.com>
2023-10-03 10:53:12 +0100

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-
+import React,{useState} from 'react';
+import Account from "./componetes/account.js"
+import Card from "./componetes/card"
 function App() {
+  const [view,setView]=useState("card")
+
+const render=()=>{
+if(view==='account'){
+  return <Account />
+}
+if(view==="card"){
+  return <Card/>
+}
+}
   return (
+    <div>
 <div>
-  <h1>hello from here</h1>
+
+{render()}
+ 
 </div>
+
+ </div>
   )
 }
 
