2206f26476d49bcb49d60db5d38ee316de100609
FediMaalej <fm3aalej@gmail.com>
2023-10-03 11:54:28 +0100

    fedi push

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
-import TopHeader from './components/TopHeader/TopHeader.js';
-import NavBar from './components/NavBar/NavBar.js'
+import "./App.css";
+import TopHeader from "./components/TopHeader/TopHeader.js";
+import NavBar from "./components/NavBar/NavBar.js";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/HomePage/Home";
+import Contact from "./components/HomePage/Contact";
 
 function App() {
   return (
-<div>
-  <TopHeader />
-  <NavBar />
-</div>
-  )
+    <>
+      <TopHeader />
+      <NavBar />
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+    </>
+  );
 }
 
 export default App;
